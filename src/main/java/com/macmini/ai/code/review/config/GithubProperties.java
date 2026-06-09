6f03ff61ec5581e38aff4964e1b855388f081d81
윤주영 <pulpul8282@naver.com>
2026-06-09 10:23:10 +0900

    chore: 프로퍼티 set 추가

--- src/main/java/com/macmini/ai/code/review/config/GithubProperties.java
+++ src/main/java/com/macmini/ai/code/review/config/GithubProperties.java
@@ -1,8 +1,10 @@
 package com.macmini.ai.code.review.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Setter
 @Getter
 @ConfigurationProperties(prefix = "github")
 public class GithubProperties {
